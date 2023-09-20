14a5da6b1bf0f7aa738b6504a17f4c58c5772b56
hiroki_mori3 <porizou.electro@gmail.com>
2023-09-20 15:22:57 +0900

    first commit

---
+++ src/dynamic_tutorials_node.cpp
@@ -0,0 +1,69 @@
+#include <rclcpp/rclcpp.hpp>
+#include <vector>
+#include <rcl_interfaces/msg/set_parameters_result.hpp>
+
+class DynamicTutorialsNode : public rclcpp::Node
+{
+public:
+  DynamicTutorialsNode()
+  : Node("dynamic_tutorials"),
+    int_param(0),
+    double_param(0.0),
+    str_param(""),
+    bool_param(false),
+    size(1)
+  {
+    this->declare_parameter<int>("int_param", int_param);
+    this->declare_parameter<double>("double_param", double_param);
+    this->declare_parameter<std::string>("str_param", str_param);
+    this->declare_parameter<bool>("bool_param", bool_param);
+    this->declare_parameter<int>("size", size);
+
+    auto parameter_change_cb = std::bind(&DynamicTutorialsNode::parameter_callback, this, std::placeholders::_1);
+    reset_param_handler_ = this->add_on_set_parameters_callback(parameter_change_cb);
+  }
+
+rcl_interfaces::msg::SetParametersResult parameter_callback(const std::vector<rclcpp::Parameter> &parameters)
+{
+  auto result = rcl_interfaces::msg::SetParametersResult();
+  result.successful = true;
+
+  for (const auto &parameter : parameters) {
+    if (parameter.get_name() == "int_param") {
+      int_param = parameter.as_int();
+      RCLCPP_INFO(this->get_logger(), "int_param changed to %d", int_param);
+    } else if (parameter.get_name() == "double_param") {
+      double_param = parameter.as_double();
+      RCLCPP_INFO(this->get_logger(), "double_param changed to %f", double_param);
+    } else if (parameter.get_name() == "str_param") {
+      str_param = parameter.as_string();
+      RCLCPP_INFO(this->get_logger(), "str_param changed to %s", str_param.c_str());
+    } else if (parameter.get_name() == "bool_param") {
+      bool_param = parameter.as_bool();
+      RCLCPP_INFO(this->get_logger(), "bool_param changed to %s", bool_param ? "true" : "false");
+    } else if (parameter.get_name() == "size") {
+      size = parameter.as_int();
+      RCLCPP_INFO(this->get_logger(), "size changed to %d", size);
+    }
+  }
+  return result;
+}
+
+private:
+  int int_param;
+  double double_param;
+  std::string str_param;
+  bool bool_param;
+  int size;
+  OnSetParametersCallbackHandle::SharedPtr reset_param_handler_;
+};
+
+int main(int argc, char **argv)
+{
+  rclcpp::init(argc, argv);
+  auto node = std::make_shared<DynamicTutorialsNode>();
+  rclcpp::spin(node);
+  rclcpp::shutdown();
+  return 0;
+}
+
